07c40a0c139aecefd0d69aaa004733b0d6fdc9e2
Namrata Taori <namratataori98+1@gmail.com>
2022-04-27 23:19:17 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql').graphqlHTTP
+const schema = require('./schema')
+
+const app = express()
+
+app.use('/graphql', expressGraphQL({
+    schema: schema,
+    graphiql: true
+}))
+
+app.listen(4000, () => {
+    console.log('Server running at port 4000..')
+})
